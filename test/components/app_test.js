eb35cc131def829554960dcf79c783c95fc29b2d
Yos Riady <yosriady@gmail.com>
2016-08-21 23:32:15 +0800

    Section 2 - 4

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,13 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+describe('App', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
+  it('shows the correct text', () => {
+
+    const component = renderComponent(App);
+    expect(component).to.contain('React simple starter');
+
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
 });
